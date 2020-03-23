7cc240b0a2a4bea44f07ace6dcb98a75e6ceb343
Elias Escobar Ramirez <eliascan@yahoo.com>
2020-03-23 00:40:54 -0400

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,25 +1,29 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-BBC_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
             'fox': 'http://feeds.foxnews.com/foxnews/latest',
             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-    feed = feedparser.parse(BBC_FEED[publication])
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEED[publication])
     first_article = feed['entries']
-    return render_template("home.html", articles=first_article)
-                            # title=first_article.get("title"), 
-                            # published=first_article.get("published"),
-                            # summary=first_article.get("summary"))
-    
+    return render_template("home.html", articles=feed['entries'])
+    # title=first_article.get("title"),
+    # published=first_article.get("published"),
+    # summary=first_article.get("summary"))
 
 
 if __name__ == '__main__':
